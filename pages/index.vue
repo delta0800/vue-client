41d8d193d740a7c54ea54803275350bed8a89885
infun-soso <973394690@qq.com>
2018-11-05 19:42:25 +0800

    fix index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -30,10 +30,10 @@
         autoplay=""
         loop="">
         <source
-          src="video/banner.mp4"
+          src="http://demo.cssmoban.com/cssthemes5/twts_139_transitive/images/banner.mp4"
           type="video/mp4">
         <source
-          src="video/banner.webm"
+          src="http://demo.cssmoban.com/cssthemes5/twts_139_transitive/images/banner.webm"
           type="video/webm">
       </video>
     </section>
@@ -51,13 +51,7 @@ export default {
 }
 </script>
 
-<style lang="less">
-html,
-body {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
+<style lang="less" scoped>
 .welcome {
   height: 100%;
   width: 100%;
