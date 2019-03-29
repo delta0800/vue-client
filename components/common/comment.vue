7456f3bf3e8568f684255046d6560f9506ee8dd5
infun-soso <973394690@qq.com>
2019-03-29 10:47:04 +0800

    hotfix.

--- components/common/comment.vue
+++ components/common/comment.vue
@@ -189,7 +189,7 @@ export default {
                 name: item.user.name,
                 avatar: item.user.avatar
               }),
-              user_id: this.$store.state.user.userInfo.user._id
+              user_id: this.$store.state.user.userInfo._id
             }
           })
           .then(result => {
